ed3165e1477bda55424e9b9490bce0b732ed5a26
sanras68 <99615241+sanras68@users.noreply.github.com>
2023-09-13 19:27:11 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var first_name = "Ranbir";
+var last_name = "Kapoor"
+
+console.log(first_name);
+console.log(last_name);
+
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a = 10;
+var b = 5;
+sum = a+b;
+console.log(sum);
+minus = a-b
+console.log(minus);
+multiply=a*b;
+console.log(multiply);
+divide=a/b;
+console.log(divide);
